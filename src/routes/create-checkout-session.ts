7441f934257c284f66a0de975f231f05bec87c19
maritzbuchholz <maritzbuchholz@gmail.com>
2026-08-11 20:05:20 -0400

    set up checkout session structure

--- src/routes/create-checkout-session.ts
+++ src/routes/create-checkout-session.ts
@@ -1,20 +1,22 @@
-// import express from "express";
-// import connection from "../mysql.js";
-// import stripe from "stripe";
-// import { v4 as uuidv4 } from 'uuid';
+import express from "express";
+import connection from "../mysql.js";
+import Stripe from "stripe";
+import { v4 as uuidv4 } from 'uuid';
 
-// const router = express.Router();
+const router = express.Router();
+const stripe = new Stripe(process.env.STRIPE_KEY as string);
 
-// router.route("/create-checkout-session").post(async (req, res) => {
-//     const session = await stripe.checkout.session.create
+router.route("/create-checkout-session").post(async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
 
-//     try {
-//         const [results] = await connection.query(sql);
-//         res.json(results);
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send("Error occured on the server");
-//     }
-// })
+    })
 
-// export default router;
+    // try {
+    //     res.json(results);
+    // } catch (error) {
+    //     console.log(error);
+    //     res.status(500).send("Error occured on the server");
+    // }
+})
+
+export default router;
